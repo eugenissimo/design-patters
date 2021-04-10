a22d0725f0d1691d96015db09731364148f6402d
Pedro Bezerra <eugenissimo@outlook.com>
2021-04-10 18:33:34 -0300

    abstract factory

---
+++ src/creational/abstractfactory/BoletoItau.java
@@ -0,0 +1,15 @@
+package creational.abstractfactory;
+
+public class BoletoItau implements Boleto {
+
+	@Override
+	public void emitir(Produto produto, double imposto) {
+		System.out.println("BANCO ITAÚ");
+		System.out.println("Descrição: " + produto.getNome());
+		System.out.println("Quantidade: " + produto.getQuantidade());
+		System.out.println("Valor unitário: " + produto.getValorUnitario());
+		System.out.println("Imposto: " + imposto);
+		System.out.println("Valor total: " + produto.getValorTotal(imposto));
+	}
+
+}
---
+++ src/creational/abstractfactory/VendaFactory.java
@@ -0,0 +1,6 @@
+package creational.abstractfactory;
+
+public interface VendaFactory {
+	public NFE criarNFE();
+	public Boleto criarBoleto();
+}
